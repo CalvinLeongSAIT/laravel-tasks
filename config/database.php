940d9cce32ec6e5cbf9b61fd14c529e0913128f4
Calvn Leong <calvin.leong@edu.sait.ca>
2025-06-11 21:30:40 +0000

    Configure Azure database and cache connections error correct

--- config/database.php
+++ config/database.php
@@ -141,7 +141,7 @@ return [
             'url' => env('REDIS_URL'),
             'host' => env('AZURE_REDIS_HOST', '127.0.0.1'),
             'username' => env('REDIS_USERNAME'),
-            'password' => env('RAZURE_REDIS_PASSWORD'),
+            'password' => env('AZURE_REDIS_PASSWORD'),
             'port' => env('AZURE_REDIS_PORT', '6379'),
             'database' => env('AZURE_REDIS_DATABASE', '1'),
             'scheme' => 'tls',
